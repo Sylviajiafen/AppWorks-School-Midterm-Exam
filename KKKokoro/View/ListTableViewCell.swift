b699acf285323197d471d1c99b14565028b3f5cd
Sylvia Jia Fen <kokoro.sharing@gmail.com>
2019-08-23 14:01:57 +0800

    UI layouts

---
+++ KKKokoro/View/ListTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ListTableViewCell.swift
+//  KKKokoro
+//
+//  Created by Sylvia Jia Fen  on 2019/8/23.
+//  Copyright © 2019 Sylvia Jia Fen . All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var albumImage: UIImageView!
+    @IBOutlet weak var songTitle: UILabel!
+    @IBOutlet weak var heartBtn: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
