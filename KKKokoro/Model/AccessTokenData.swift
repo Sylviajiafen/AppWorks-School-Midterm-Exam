17d01f0ddb862d5d5f8e3ae7bedec5c56609c868
Sylvia Jia Fen <kokoro.sharing@gmail.com>
2019-08-23 10:10:23 +0800

    create

---
+++ KKKokoro/Model/AccessTokenData.swift
@@ -0,0 +1,27 @@
+//
+//  AccessTokenData.swift
+//  KKKokoro
+//
+//  Created by Sylvia Jia Fen  on 2019/8/23.
+//  Copyright © 2019 Sylvia Jia Fen . All rights reserved.
+//
+
+import Foundation
+
+struct AccessToken: Codable {
+    
+    let accessToken: String
+    
+    let tokenType: String
+    
+    let expiresIn: Int
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case accessToken = "access_token"
+        
+        case tokenType = "token_type"
+        
+        case expiresIn = "expires_in"
+    }
+}
---
+++ KKKokoro/Model/HotListData.swift
@@ -0,0 +1,134 @@
+//
+//  ModelData.swift
+//  KKKokoro
+//
+//  Created by Sylvia Jia Fen  on 2019/8/23.
+//  Copyright © 2019 Sylvia Jia Fen . All rights reserved.
+//
+
+import Foundation
+
+struct HitList: Codable {
+    
+    let data: [String]
+    
+    let paging: Paging
+    
+    let summary: Summary
+}
+
+struct Data: Codable {
+    
+    let id: String
+    
+    let name: String
+    
+    let duration: Int
+    
+    let url: String
+    
+    let trackNumber: Int
+    
+    let explicitness: Bool
+    
+    let availableTerritories: [String]
+    
+    let album: Album
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case id
+        
+        case name
+        
+        case duration
+        
+        case url
+        
+        case trackNumber = "track_number"
+        
+        case explicitness
+        
+        case availableTerritories = "available_territories"
+        
+        case album
+    }
+    
+}
+
+struct Album: Codable {
+    
+    let id: String
+    
+    let name: String
+    
+    let url: String
+    
+    let explicitness: Bool
+    
+    let availableTerritories: [String]
+    
+    let releaseDate: String
+    
+    let images: [Image]
+    
+    let artists: Artist
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case id
+        
+        case name
+        
+        case url
+        
+        case explicitness
+        
+        case availableTerritories = "available_territories"
+        
+        case releaseDate = "release_date"
+        
+        case images
+        
+        case artists
+        
+    }
+    
+}
+
+struct Image: Codable {
+    
+    let height: Int
+    
+    let width: Int
+    
+    let url: String
+}
+
+struct Artist: Codable {
+    
+    let id: String
+    
+    let name: String
+    
+    let url: String
+    
+    let images: [Image]
+}
+
+struct Paging: Codable {
+    
+    let offset: Int
+    
+    let limit: Int
+    
+//    let previous:
+    
+    let next: String
+    
+}
+
+struct Summary: Codable {
+    
+    let total: Int
+}
